4693c84e41a5b8f94554132855584bbb1c8e2757
Recai Oktaş <roktas@bil.omu.edu.tr>
2011-01-10 08:11:09 +0200

    hmm

--- lib/sh/package.sh
+++ lib/sh/package.sh
@@ -1,28 +1,36 @@
-# aptitude sarmalayıcı
-apti() {
+# apt-get sarmalayıcı
+apt() {
 	local command
-	command="$1"
-	shift 2>/dev/null ||:
-	sudo aptitude "$command" -f --quiet --assume-yes --without-recommends "$@"
-}
+	command="$1"; shift 2>/dev/null ||:
 
-# deb paketlerini etkileşimsiz şekilde apt-get ile kur
-aptinstall() {
-	sudo apt-get install --option APT::Get::HideAutoRemove=1 \
+	sudo apt-get $command --option APT::Get::HideAutoRemove=1 \
 		--quiet --yes --force --no-install-recommends --ignore-missing "$@"
 }
 
-# gem kur (sistem öntanımlısıyla)
-geminstall() {
-	sudo gem install --quiet "$@"
+# aptitude sarmalayıcı
+apti() {
+	local command
+	command="$1"; shift 2>/dev/null ||:
+	sudo aptitude $command -f --quiet --assume-yes --without-recommends "$@"
 }
 
-# gem kur (1.9 serisi ile)
-gem191install() {
-	sudo gem1.9.1 install --quiet "$@"
+# gem sarmalayıcı (öntanımlı)
+gem() {
+	local command
+	command="$1"; shift 2>/dev/null ||:
+	sudo gem $command --quiet "$@"
 }
 
-# wajig için sarmalayıcı
+# gem sarmalayıcı (1.9 serisi ile)
+gem191() {
+	local command
+	command="$1"; shift 2>/dev/null ||:
+	sudo gem1.9.1 $command --quiet "$@"
+}
+
+# wajig sarmalayıcı
 wajig() {
-	local url; for url; do wajig -n -q -y install "$url"; done
+	local command
+	command="$1"; shift 2>/dev/null ||:
+	wajig -n -q -y $command "$@"
 }
